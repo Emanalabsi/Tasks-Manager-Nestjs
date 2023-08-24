4e3da52cb81e0dc4cf070b65a0d37d4c753f07a7
Emanalabsi <emanabsi30@gmail.com>
2023-08-24 19:37:50 +0300

    feat: implement tasks provider

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTaskDto } from './dto/task-create.dto';
+CreateTaskDto;
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class TasksService {
+  constructor(
+    @InjectRepository(Task) private readonly taskRepository: Repository<Task>,
+  ) {}
+
+  getTasks() {
+    return this.taskRepository.find();
+  }
+
+  addTask(createTaskDto: CreateTaskDto) {
+    const newTask = this.taskRepository.create(createTaskDto);
+    return this.taskRepository.save(newTask);
   }
 }
